59198778d92681a67e8166833d2e0d719034fcc2
RyotaFukumoto <k014c1106@it-neec.jp>
2019-10-26 10:08:15 +0900

    FirstCommit

---
+++ Youtube Viewer/Youtube Viewer/Other/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Youtube Viewer
+//
+//  Created by ryota on 2019/10/26.
+//  Copyright © 2019 ryota. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
